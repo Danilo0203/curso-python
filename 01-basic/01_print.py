19b04750c3e14813beee565de269fc81e8959403
Danilo0203 <danilocalderon0203@gmail.com>
2026-02-05 09:07:06 -0600

    Mi primer hola mundo en python

---
+++ 01-basic/01_print.py
@@ -0,0 +1,12 @@
+nombre = 'Danilo' 
+print('Hola mundo!')
+
+
+# Cambiar los espacios entre palabras, sep
+print('Cambien los espaciones entre cada palabra', sep="-")
+print('Cambia', 'los', 'espacios', sep="-")
+
+
+# Cambio de saltos de linea, end
+print('Es es la primera linea', end=" ")
+print('Sigue en la segunda linea')
